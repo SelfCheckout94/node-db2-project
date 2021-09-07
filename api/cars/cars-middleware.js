099242f9c39efd3051a1c6293ed4c354ccb496f8
Christopher Boyle <cboyle824@gmail.com>
2021-09-07 12:45:21 -0400

    MVP complete

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,70 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const { getById, getAll } = require("./cars-model");
+const vinValidator = require("vin-validator");
+
+const checkCarId = async (req, res, next) => {
+  try {
+    const car = await getById(req.params.id);
+    if (car) {
+      req.car = car;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: `car with id ${req.params.id} is not found`,
+      });
+    }
+  } catch (err) {
+    next();
+  }
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) {
+    next({
+      status: 400,
+      message: "vin is missing",
+    });
+  } else if (!make) {
+    next({
+      status: 400,
+      message: "make is missing",
+    });
+  } else if (!model) {
+    next({
+      status: 400,
+      message: "model is missing",
+    });
+  } else if (!mileage) {
+    next({
+      status: 400,
+      message: "mileage is missing",
+    });
+  } else {
+    next();
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin } = req.body;
+  const isValidVin = vinValidator.validate(vin);
+  isValidVin ? next() : next({ status: 400, message: `vin ${vin} is invalid` });
+};
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body;
+  const cars = await getAll();
+  const existingVin = cars.find((car) => {
+    return car.vin === vin;
+  });
+  !existingVin
+    ? next()
+    : next({ status: 400, message: `vin ${vin} already exists` });
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,49 @@
-// DO YOUR MAGIC
+const express = require("express");
+const {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+} = require("./cars-middleware");
+const Cars = require("./cars-model");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.status(200).json(cars);
+  } catch (err) {
+    next();
+  }
+});
+
+router.get("/:id", checkCarId, (req, res) => {
+  res.status(200).json(req.car);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid,
+  async (req, res, next) => {
+    try {
+      const newCar = await Cars.create(req.body);
+      res.status(200).json(newCar);
+    } catch (err) {
+      next();
+    }
+  }
+);
+
+// eslint-disable-next-line
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    custom: "Something wen wrong with the cars router",
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
